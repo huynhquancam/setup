ad601f2e3adea66428cf4b0cb4affa981988487b
Cẩm Huỳnh <huynhquancam@gmail.com>
2017-01-17 21:44:32 +0100

    Update script

--- laptop.sh
+++ laptop.sh
@@ -13,24 +13,34 @@ info_log () {
 install_log "Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-install_log "Git"
-brew install git
-
 info_log "Upgrade Homebrew"
 brew upgrade
+
+install_log "Git"
+brew install git hub
 
 install_log "Vim, ZSH and Dotfiles"
 brew install vim zsh
 git clone https://github.com/huynhquancam/dotfiles.git ~/dotfiles && ~/dotfiles/bin/bootstrap.sh
 
+install_log "Setting zsh as default shell"
+command -v zsh | sudo tee -a /etc/shells
+chsh -s $(which zsh)
+
+install_log "dev stuff"
+brew install rbenv ruby-build elixir
+brew install vim zsh tmux
+git clone https://github.com/huynhquancam/dotfiles.git ~/dotfiles && ~/dotfiles/bin/bootstrap.sh
+
 install_log "CLI Tool Utilities"
-brew install tree the_silver_searcher ctags tree jq httpie
+brew install tree the_silver_searcher ctags tree jq httpie keybase reattach-to-user-namespace
+gem install worque
 
 install_log "Mac CLI"
 git clone https://github.com/guarinogabriel/mac-cli.git && sh mac-cli/mac install
 
 install_log "Node"
-node npm nvm zsh
+brew install node npm nvm zsh
 npm install -g http
 
 install_log "Homebrew Cash"
@@ -39,6 +49,5 @@ brew tap caskroom/cask
 install_log "Docker For Mac"
 brew cask install docker
 
-install_log "Mac Apps"
-brew cask install google-chrome skype iterm2 sourcetree spectacle
-
+install_log "Mac Apps" 
+brew cask install google-chrome skype iterm2 sourcetree spectacle slack dropbox 1password
