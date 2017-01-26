3215a40cb0829a859e1911f8f82848023ee6f35d
Cẩm Huỳnh <huynhquancam@gmail.com>
2017-01-26 22:14:40 +0100

    Update laptop.sh

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# setup
-My Mac set up script
+setup
+===
+
+One-off MacOS setup script
+
+To install run
+
+```sh
+curl -L https://raw.githubusercontent.com/qcam/setup/master/laptop.sh
+```
--- laptop.sh
+++ laptop.sh
@@ -19,18 +19,14 @@ brew upgrade
 install_log "Git"
 brew install git hub
 
-install_log "Vim, ZSH and Dotfiles"
-brew install vim zsh
-git clone https://github.com/huynhquancam/dotfiles.git ~/dotfiles && ~/dotfiles/bin/bootstrap.sh
+install_log "Dev stuff"
+brew install vim zsh tmux
+brew install rbenv ruby-build elixir
+git clone https://github.com/qcam/dotfiles.git ~/dotfiles && ~/dotfiles/bin/bootstrap.sh
 
 install_log "Setting zsh as default shell"
 command -v zsh | sudo tee -a /etc/shells
 chsh -s $(which zsh)
-
-install_log "dev stuff"
-brew install rbenv ruby-build elixir
-brew install vim zsh tmux
-git clone https://github.com/huynhquancam/dotfiles.git ~/dotfiles && ~/dotfiles/bin/bootstrap.sh
 
 install_log "CLI Tool Utilities"
 brew install tree the_silver_searcher ctags tree jq httpie keybase reattach-to-user-namespace
@@ -49,5 +45,5 @@ brew tap caskroom/cask
 install_log "Docker For Mac"
 brew cask install docker
 
-install_log "Mac Apps" 
+install_log "Mac Apps"
 brew cask install google-chrome skype iterm2 sourcetree spectacle slack dropbox 1password
